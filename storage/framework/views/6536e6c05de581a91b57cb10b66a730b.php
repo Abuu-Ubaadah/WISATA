f020cbc92d62139f99254c92833b99c84d76c5ac
أبو عبادة <aldyfwkymhmd@gmail.com>
2025-02-11 13:08:08 +0700

    Add files via upload

---
+++ storage/framework/views/6536e6c05de581a91b57cb10b66a730b.php
@@ -0,0 +1,4 @@
+  <div class="simple-footer">
+      Copyright &copy; Stisla 2018
+  </div>
+<?php /**PATH D:\laragon\www\wisata-idn\resources\views/components/auth-footer.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/92fcff28316f25a844698adf6cb59b4f.php
@@ -0,0 +1,39 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">WISATA BAHARI</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="nav-item  ">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+
+            </li>
+
+            <li class="nav-item ">
+                <a href="<?php echo e(route('users.index')); ?>" class="nav-link "><i class="fas fa-columns"></i>
+                    <span>Users</span></a>
+            </li>
+
+            <li class="nav-item ">
+                <a href="<?php echo e(route('categories.index')); ?>" class="nav-link "><i class="fas fa-columns"></i>
+                    <span>Categories</span></a>
+            </li>
+
+            <li class="nav-item ">
+                <a href="<?php echo e(route('products.index')); ?>" class="nav-link "><i class="fas fa-columns"></i>
+                    <span>Tickets</span></a>
+            </li>
+
+            <li class="nav-item ">
+                <a href="<?php echo e(route('orders.index')); ?>" class="nav-link "><i class="fas fa-columns"></i>
+                    <span>Orders</span></a>
+            </li>
+
+
+
+    </aside>
+</div>
+<?php /**PATH D:\laragon\www\wisata-idn\resources\views/components/sidebar.blade.php ENDPATH**/ ?>
